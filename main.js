f7a8b40257ecfed09b3b4f7ea658458f910d62a9
Anton Kapustin <85716128+antonkapustin@users.noreply.github.com>
2025-03-24 17:32:59 +0300

    minor fix

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { App } from './components/app.js';
 
-window.Telegram.WebApp.ready();
+const tg = window.Telegram.WebApp;
+
+tg.ready();
 const appElement = document.getElementById('app');
 const app = new App(appElement);
